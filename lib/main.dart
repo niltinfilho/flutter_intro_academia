ce10e7a95387a39d4c69cd38d78d1d5ab73dcd30
Nilton Filho <niltonfilho115@gmail.com>
2025-08-07 11:17:05 -0300

    feat: criando rotas nomeadas

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_intro_academia/hello_world/hello_world_screen.dart';
+import 'package:flutter_intro_academia/scaffold_example/scaffold_screen.dart';
 import 'package:flutter_intro_academia/stateful_widget_example/stateful_widget_screen.dart';
+import 'package:flutter_intro_academia/widgets_basicos/widgets_basicos_screen.dart';
 
 void main() {
   runApp(FlutterIntroApp());
@@ -18,7 +21,72 @@ class FlutterIntroApp extends StatelessWidget {
       // home: HelloWorldScreen(),
       // home: ScaffoldScreen(),
       // home: WidgetsBasicosScreen(),
-      home: StatefulWidgetScreen(),
+      // home: StatefulWidgetScreen(),
+      routes: {
+        '/': (context) => MenuPrincipal(),
+        '/hello': (context) => HelloWorldScreen(),
+        '/scaffold': (context) => ScaffoldScreen(),
+        '/stateful': (context) => StatefulWidgetScreen(),
+        '/widget-basico': (context) => WidgetsBasicosScreen(),
+      },
+    );
+  }
+}
+
+class MenuPrincipal extends StatelessWidget {
+  const MenuPrincipal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Card(
+              margin: EdgeInsets.all(10),
+              elevation: 3,
+              child: ListTile(
+                onTap: () {
+                  Navigator.of(context).pushNamed('/hello');
+                },
+                title: Text('Hello Word'),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.all(10),
+              elevation: 3,
+              child: ListTile(
+                onTap: () {
+                  Navigator.of(context).pushNamed('/scaffold');
+                },
+                title: Text('Scaffold'),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.all(10),
+              elevation: 3,
+              child: ListTile(
+                onTap: () {
+                  Navigator.of(context).pushNamed('/stateful');
+                },
+                title: Text('Stateful Widget'),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.all(10),
+              elevation: 3,
+              child: ListTile(
+                onTap: () {
+                  Navigator.of(context).pushNamed('/widget-basico');
+                },
+                title: Text('Widget Básico'),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
